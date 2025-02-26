9c5e81f5b5670e23c0229668efd7491ed8f3a63c
Alejandro Chavazza <alejandrochavazza@gmail.com>
2025-02-26 12:19:55 +0100

    layout

--- resources/js/Components/MenuItem.vue
+++ resources/js/Components/MenuItem.vue
@@ -1,19 +1,10 @@
 <template>
-    <div class="
-        w-full
-        lg:inline-block
-        hover:bg-gray-900
-        9-2
-        px-4
-        lg:pr-6
-        rounded-full
-        cursor-pointer
-        transition
-        duration-200
-        ease-in-out">
+    <div class="w-full lg:inline-block hover:bg-gray-900 p-2 px-4 lg:pr-6 rounded-full cursor-pointer transition duration-200 ease-in-out">
         <div class="flex items-center">
-            <component :is="icon" fillColor="#fff" size="30" />
-            <span class="lg:block hidden text-white font-extrabold text-[23px] pl-4 mt-0.5">{{ iconString }}</span>
+            <component :is="icon" fillColor="#FFFFFF" :size="30" />
+            <span class="lg:block hidden text-white font-extrabold text-[23px] pl-4 mt-0.5">
+                {{ iconString }}
+            </span>
         </div>
     </div>
 </template>
